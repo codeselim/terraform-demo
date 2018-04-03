09ae67ce4e997101b9a1318cf1c7803fb55b63ea
sasa <selimabisaber@gmail.com>
2018-04-03 19:04:43 +0200

    seperating enviornments

---
+++ development/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  default = "eu-central-1"
+  description = "The AWS region."
+}
+
+variable "environment" {
+  default = "development"
+  description = "The name of our environment, i.e. development."
+}
+
---
+++ production/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "eu-central-1"
+  description = "The AWS region."
+}
+
+variable "environment" {
+  default = "production"
+  description = "The name of our environment, i.e. development."
+}
